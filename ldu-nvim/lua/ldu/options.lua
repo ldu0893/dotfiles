022ff42fd2fede035bd09349fb0970269c7a69c8
Larry Du <larry@larrydu.com>
2026-05-04 16:33:58 -0700

    nvim copy on double triple click tmux styling

--- ldu-nvim/lua/ldu/options.lua
+++ ldu-nvim/lua/ldu/options.lua
@@ -29,6 +29,16 @@ vim.g.clipboard = {
 
 -- On mouse release in visual mode, yank selection to system clipboard
 vim.keymap.set("v", "<LeftRelease>", '"+y', { noremap = true })
+local function yank_after_delay(select_cmd, delay)
+  return function()
+    vim.cmd("normal! " .. select_cmd)
+    vim.defer_fn(function()
+      vim.cmd('normal! "+y')
+    end, delay)
+  end
+end
+vim.keymap.set("n", "<2-LeftMouse>", yank_after_delay("viw", 150), { noremap = true })
+vim.keymap.set("n", "<3-LeftMouse>", yank_after_delay("V", 350), { noremap = true })
 
 -- Disable expensive features for large files (>1 MB)
 vim.api.nvim_create_autocmd("BufReadPre", {
